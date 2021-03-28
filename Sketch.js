c085f6cc14bb1e50a06f199981a96ef4c07457bd
nicklbaert <niclasillg@gmail.com>
2021-03-28 15:20:26 +0200

    styling bitch

--- Sketch.js
+++ Sketch.js
@@ -8,32 +8,16 @@ function setSystem(newSystem) {
 }
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(700, 400);
     slider = createSlider(0, 10, 5, 1);
-
-    var button;
-    button = createButton('Fractal Binary Tree');
-    button.mousePressed(() => setSystem(new FractalBinaryTree()));
-
-    button = createButton('Sierpinski Triangle');
-    button.mousePressed(() => setSystem(new SierpinskiTriangle()));
-
-    button = createButton('Sierpinski Arrowhead Curve');
-    button.mousePressed(() => setSystem(new SierpinskiArrowheadCurve()));
-
-    button = createButton('Dragon Curve');
-    button.mousePressed(() => setSystem(new DragonCurve()));
-
-    button = createButton('Fractal Plant');
-    button.mousePressed(() => setSystem(new FractalPlant()));
 
     setSystem(new FractalBinaryTree())
 }
 
 function draw() {
     if (slider.value() != lastSliderValue) {
-        background(220);
-        stroke(0);
+        background(60, 64, 73);
+        stroke(0, 255, 255);
         strokeWeight(2);
         angleMode(DEGREES);
         system.display(slider.value());
